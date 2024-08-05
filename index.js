883aff5e7ed936ace6ac545cb15dc706aae885b9
Samuel Johnson <s3johnson7@gmail.com>
2024-08-05 03:08:45 +0000

    Initial commit

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.acceptsLanguages(),
+    "software": req.acceptsEncodings()
+  });
+  console.log(req.acceptsLanguages());
+});
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
